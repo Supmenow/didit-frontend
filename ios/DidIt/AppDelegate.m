be8e0a48cc4ac847563c28351478b4df0fc46ace
James Campbell <james@supmenow.com>
2016-08-08 17:20:35 +0100

    Don't set a color.

--- ios/DidIt/AppDelegate.m
+++ ios/DidIt/AppDelegate.m
@@ -32,7 +32,6 @@
                                                       moduleName:@"DidIt"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
