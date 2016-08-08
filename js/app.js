c91f75150e5d1cf0b18983d0e20df2842783b28b
James Campbell <james@supmenow.com>
2016-08-08 17:16:46 +0100

    Notification magic.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import FCM from 'react-native-fcm';
 
 import Login from './components/login';
 import Signup from './components/signup';
@@ -24,6 +25,34 @@ class App extends Component {
     if (this.props.profile) {
       this.props.dispatch(uploadContacts(this.props.profile["api-key"]));
     }
+  }
+
+  componentDidMount() {
+    FCM.requestPermissions(); // for iOS
+  }
+
+  FCM.getFCMToken().then(token => {
+      console.log(token)
+      // store fcm token in your server
+    });
+
+    this.notificationUnsubscribe = FCM.on('notification', (notif) => {
+      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+    });
+    
+    this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
+      console.log(token)
+      // fcm token may not be available on first load, catch it here
+    });
+    //
+    // FCM.subscribeToTopic('/topics/foo-bar');
+    // FCM.unsubscribeFromTopic('/topics/foo-bar');
+  }
+
+  componentWillUnmount() {
+    // prevent leaking
+    this.refreshUnsubscribe();
+    this.notificationUnsubscribe();
   }
 
   render() {
