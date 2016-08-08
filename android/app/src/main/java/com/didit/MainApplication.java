23497061304ad287d85aba05445d92de2bcc8328
James Campbell <james@supmenow.com>
2016-08-08 17:09:16 +0100

    Implement FCM.

--- android/app/src/main/java/com/didit/MainApplication.java
+++ android/app/src/main/java/com/didit/MainApplication.java
@@ -23,8 +23,6 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
 
-//      ReactExplosionCoordinator explosionCoordinator = ReactExplosionCoordinator()
-
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
               new DigitsPackage(),
