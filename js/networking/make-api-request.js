cc8e1497111a054a5813824ee4240340c7e38aa5
James Campbell <james@supmenow.com>
2016-08-11 15:16:09 +0100

    Additional logging.

--- js/networking/make-api-request.js
+++ js/networking/make-api-request.js
@@ -14,6 +14,8 @@ function parseAPIResponse(json) {
 
 function makeAPIRequest(path, data, key, method = 'POST') {
 
+    var url = 'http://139.59.184.179/api/v1' + path
+
     var headers = {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
@@ -24,10 +26,17 @@ function makeAPIRequest(path, data, key, method = 'POST') {
       headers["api-key"] = key
     }
 
-    return fetch('http://139.59.184.179/api/v1' + path, {
+    var body = JSON.stringify(data)
+
+    console.log("URL: " + url)
+    console.log("Method: " + method)
+    console.log("Headers: " + headers)
+    console.log("Body: " + body)
+
+    return fetch(url, {
       method: method,
       headers: headers,
-      body: JSON.stringify(data)
+      body: body
     })
     .then(function(response) {
       return response.json();
