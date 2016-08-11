f69a81c7c2674e628fffde2a6d03a9221cce0ae9
James Campbell <james@supmenow.com>
2016-08-11 08:22:25 +0100

    Handle pop gesture.

--- js/transition-navigation.js
+++ js/transition-navigation.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Navigator } from 'react-native';
 
-// FIXME: Handle pop gesture with state
 class TransitionNavigation extends Component {
 
   constructor(props) {
