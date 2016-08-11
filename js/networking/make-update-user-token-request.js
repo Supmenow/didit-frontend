cd8fbe476a161058b79bd4611dcff927753a418c
James Campbell <james@supmenow.com>
2016-08-11 16:28:13 +0100

    Upload token.

--- js/networking/make-update-user-token-request.js
+++ js/networking/make-update-user-token-request.js
@@ -1,7 +1,6 @@
 import { makeAPIRequest } from './make-api-request';
 
 function makeUpdateUserTokenRequest(apiKey, token) {
-  alert(protocol);
   return makeAPIRequest('/users', {
     "device_token": token,
     "proto": protocol
