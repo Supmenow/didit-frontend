f956ed85bf86382f76ec8ee89f21fd34cc82c684
James Campbell <james@supmenow.com>
2016-08-11 16:30:07 +0100

    Fixes error.

--- js/networking/make-update-user-token-request.js
+++ js/networking/make-update-user-token-request.js
@@ -1,6 +1,6 @@
 import { makeAPIRequest } from './make-api-request';
 
-function makeUpdateUserTokenRequest(apiKey, token) {
+function makeUpdateUserTokenRequest(apiKey, token, protocol) {
   return makeAPIRequest('/users', {
     "device_token": token,
     "proto": protocol
