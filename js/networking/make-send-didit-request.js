e13148b92498159aad04c64f4dcbe9d6746056bb
James Campbell <james@supmenow.com>
2016-08-12 16:27:27 +0100

    Preparing new payload.

--- js/networking/make-send-didit-request.js
+++ js/networking/make-send-didit-request.js
@@ -1,7 +1,11 @@
 import { makeAPIRequest } from './make-api-request';
 
 function makeSendDidItRequest(apiKey) {
-  return makeAPIRequest('/send', {}, apiKey);
+  return makeAPIRequest('/send', {
+    'message': 'just did it',
+    'image': 'smiley',
+    'sound': 'dong.wav'
+  }, apiKey);
 }
 
 module.exports = {
