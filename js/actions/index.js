f51c47234fae9b3c0ed4093014368d65acd9029b
James Campbell <james@supmenow.com>
2016-08-31 12:46:46 +0100

    Epxose aciton.

--- js/actions/index.js
+++ js/actions/index.js
@@ -3,11 +3,13 @@ import signUpWithName from './sign-up-with-name';
 import sendDidIt from './send-didit';
 import sendReply from './send-reply';
 import uploadContacts from './upload-contacts';
+import registerForNotifications from './register-for-notifications';
 
 module.exports = {
   ...loginWithDigits,
   ...signUpWithName,
   ...sendDidIt,
   ...sendReply,
-  ...uploadContacts
+  ...uploadContacts,
+  ...registerForNotifications
 };
